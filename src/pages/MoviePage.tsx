bf6a3c40fbc21241364826571a7d24a0a0bb3ee5
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-09-07 20:50:02 +0200

    .

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import { useMovieId } from '../hooks/useMovieIdHook';
 import { RowCard } from '../components/RowActorCard';
 import { IActorResponse } from '../types/Actor.types';
-
+import useViewed from '../hooks/useViewedHook';
 import { LSContext, StoreDataType } from '../contexts/localstorageContext';
 import { useContext, useEffect } from 'react';
 
@@ -14,73 +14,88 @@ type IdParam = {
 };
 
 const Movie = () => {
+  const { viewed }: StoreDataType = useViewed();
   const { storedValue } = useContext(LSContext);
-
+  console.log(viewed);
   const { id } = useParams<IdParam>();
   const idValue = id ?? '';
 
   const { data, isError, isLoading, isSuccess } = useMovieId(idValue);
   useEffect(() => {
-    storedValue(data?.title, data?.id, data?.poster_path);
+    if (data && !viewed.some((movie) => movie.id === data.id)) {
+      storedValue(data.title, data.id, data.poster_path);
+    }
   }, [data]);
-  return (
-    <>
-      {isError && <Alert variant="warning">Oops, something went wrong!</Alert>}
-      {isLoading && (
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      )}
+return (
+  <>
+    {isError && <Alert variant="warning">Oops, something went wrong!</Alert>}
+    {isLoading && (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    )}
 
-      {data && isSuccess && (
-        <>
-          <h1>{data.title}</h1>
-          <ListGroup className="mb-6">
-            <Container>
-              <Row>
-                <MovieCard
-                  key={data?.id}
-                  poster_path={data?.poster_path}
-                  title={data?.title}
-                  overview={data?.overview}
-                  release_date={data?.release_date}
-                  vote_average={data?.vote_average}
-                  id={data?.id}
+    {data && isSuccess && (
+      <>
+        <h1>{data.title}</h1>
+        <ListGroup className="mb-6">
+          <Container>
+            <Row>
+              <MovieCard
+                key={data?.id}
+                poster_path={data?.poster_path}
+                title={data?.title}
+                overview={data?.overview}
+                release_date={data?.release_date}
+                vote_average={data?.vote_average}
+                id={data?.id}
+              />
+            </Row>
+          </Container>
+          <h2>The Cast</h2>
+
+          {isSuccess && data.credits?.cast && (
+            <>
+              <h2>Also featured in: </h2>
+              {data?.credits?.cast.map((person: IActorResponse) => (
+                <RowMovieCard
+                  id={person.id}
+                  profile_path={person.profile_path}
+                  name={person.name}
                 />
-              </Row>
-            </Container>
-            <h2>The Cast</h2>
+              ))}
+            </>
+          )}
+          {viewed.length > 0 && (
+            <div className="grid grid-cols-1 items-center justify-center px-4 md:px-8">
+              {viewed.map((item: any) => (
+                <RowCard
+                  key={item.id} // Add the key prop
+                  id={item.id!}
+                  poster_path={item.poster_path ?? 'Poster is missing'}
+                  title={item.title ?? 'title is missing'}
+                />
+              ))}
+            </div>
+          )}
 
-            {isSuccess && data.credits?.cast && (
-              <>
-                <h2>Also featured in: </h2>
-                {data?.credits?.cast.map((person: IActorResponse) => (
-                  <RowMovieCard
-                    id={person.id}
-                    profile_path={person.profile_path}
-                    name={person.name}
-                  />
-                ))}
-              </>
-            )}
-
-            <h2>Similar Movies</h2>
-            {isSuccess && data.similar?.results && (
-              <>
-                {data.similar?.results.map((movie) => (
-                  <RowCard
-                    id={movie.id!}
-                    poster_path={movie.poster_path ?? 'Poster is missing'}
-                    title={movie.title ?? 'title is missing'}
-                  />
-                ))}
-              </>
-            )}
-          </ListGroup>
-        </>
-      )}
-    </>
-  );
+          <h2>Similar Movies</h2>
+          {isSuccess && data.similar?.results && (
+            <>
+              {data.similar?.results.map((movie) => (
+                <RowCard
+                  id={movie.id!}
+                  poster_path={movie.poster_path ?? 'Poster is missing'}
+                  title={movie.title ?? 'title is missing'}
+                />
+              ))}
+            </>
+          )}
+        </ListGroup>
+      </>
+    )}
+  </>
+);
 };
 
 export default Movie;
