55f6647e6ecc7acf3ff168d4d36fe3cc5a8d9ec5
ali01 <astrid.lindfors@findity.com>
2023-10-05 15:38:52 +0200

    next page in genre works

--- src/hooks/useGenreHook.ts
+++ src/hooks/useGenreHook.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getGenre } from '../services/TMDB';
 
 const useGenre = (idValue: string, page: number) => {
-  return useQuery(['Genre/', idValue], () => getGenre(idValue, page), {
+  return useQuery(['Genre', idValue, page], () => getGenre(idValue, page), {
     enabled: !!idValue,
     keepPreviousData: true
   });
